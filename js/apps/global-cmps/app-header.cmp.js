09fc029770ee39fd58d0185949e8fcde2eb6e60c
9Syncopations <9syncopation@gmail.com>
2019-11-06 14:50:23 +0200

    changing base

--- js/apps/global-cmps/app-header.cmp.js
+++ js/apps/global-cmps/app-header.cmp.js
@@ -1,7 +1,7 @@
 'use strict'
 export default {
     template: `
-            <section>
+            <section class ="header flex center">
                <nav>
                     <router-link to="/">Home</router-link> |
                     <router-link to="/email">Email App</router-link> |
