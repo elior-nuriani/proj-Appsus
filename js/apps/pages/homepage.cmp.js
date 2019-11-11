ca798b7ae05557da9791067fb8319aab11177e7d
Elior Nuri <eliornuri777@gmail.com>
2019-11-11 03:54:18 +0200

    contact form

--- js/apps/pages/homepage.cmp.js
+++ js/apps/pages/homepage.cmp.js
@@ -12,7 +12,28 @@ export default {
                         <i class="fas fa-pen"></i>
                     </div>
                     <aside class="offcanvas-aside" :class="toggleClassAside">
-                        
+                    <section id="contact" class="flex column align-center">
+                    <h4>Contact Us</h4>
+                    <form>
+                      <div class="form-group">
+                        <label for="exampleFormControlInput1">Email address</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                      </div>
+                      <div class="form-group">
+                        <label for="subject">Subjet</label>
+                        <input type="text" class="form-control" id="subject" placeholder="Contact me" autocomplete="off">
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Body Message</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                      </div>
+                      <div class="contact-btn flex space-evenly">
+                        <button type="button" class="btn btn-primary" @click="sendContact">Submit</button>
+                        <button type="button" class="btn btn-warning" @click="toggleContact">Cancel</button>
+                      </div>
+                    </form>
+                  </section>
+              
                     </aside>
                 </section>`
     ,
@@ -24,6 +45,13 @@ export default {
     methods:{
         toggleContact(){
             this.isOpenContact = !this.isOpenContact
+        },
+        sendContact(){
+            Swal.fire(
+                'Thank You For Contact Us ',
+                '',
+                'success'
+              )
         }
     },
     computed:{
